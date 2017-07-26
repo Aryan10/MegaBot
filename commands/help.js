73ae39c109f6b7ffad35b818bb5e541a6c06c59b
Aryan10 <aryan.shivam.10@gmail.com>
2017-07-26 08:41:50 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,5 +1,5 @@
 const config = require('../config.json');
 exports.run = (client, message, args) => {
-const commandslist = "help\nsay\nping\n"
+const commandslist = "help\nsay\nping\ngroupinfo\n"
 message.channel.send(`List Of Commands\n*(Prefix = ${config.prefix})*\n\n\`\`\`\n${commandslist}\`\`\``);
 }
