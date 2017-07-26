2fbd7e3a0d0432ddd6d5b598ac5dda1386c48300
Aryan10 <aryan.shivam.10@gmail.com>
2017-07-26 09:43:07 +0530

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -5,10 +5,10 @@ const Discord = require("discord.js");
 const groupMember = message.author;
 
 const embed = new Discord.RichEmbed()
-.setTitle(groupMember.name)
+.setTitle(groupMember.username)
 .setThumbnail(groupMember.avatarURL)
 .setColor(3447003)
-.addField(`User Name`, `${groupMember.username}_(${groupMember.tag})_`)
+.addField(`User Name`, `${groupMember.username} (${groupMember.tag})`)
 .addField("User ID", groupMember.id)
 .addField("Joined Discord", groupMember.createdAt)
 .addField(`Other Info`, `Status: ${groupMember.presence.status}\nDiscrim: ${groupMember.discriminator}\nBot: ${groupMember.bot}`);
