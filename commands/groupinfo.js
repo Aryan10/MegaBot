3d4495c317646e95b74f61b33115b5033ac18f60
Aryan10 <aryan.shivam.10@gmail.com>
2017-07-26 09:03:36 +0530

    Update groupinfo.js

--- commands/groupinfo.js
+++ commands/groupinfo.js
@@ -5,8 +5,10 @@ const embed = new Discord.RichEmbed()
 .setTitle("Group Info")
 .setColor(4447003)
 .setThumbnail(`https://cdn.discordapp.com/icons/${group.id}/${group.icon}.png`)
+.setFooter(`<#${group.id}>`)
 .addField("Group Name", group.name)
-.addField("Group Owner", group.owner.username)
+.addField("Group ID", group.id)
+.addField("Group Owner", group.owner.tag)
 .addField("Members", group.recipients.map(m => m.username).join(", "))
 .addField("Created At", group.createdAt);
 message.channel.send({embed});
